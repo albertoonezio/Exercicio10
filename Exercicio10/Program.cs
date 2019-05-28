8182d7273109e323a5f08a1048fab768e1c8077d
Alberto <albertomaronezio@hotmai.com>
2019-05-28 16:59:51 -0300

    Exercicio10

---
+++ Exercicio10/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2 = 0;
+            int resultado = 0;
+            Console.Write("Digite o Primeiro Número: ");
+            n1 = int.Parse(Console.ReadLine());
+            Console.Write("Digite o Segundo Número: ");
+            n2 = int.Parse(Console.ReadLine());
+            resultado = n1 * n2;
+            Console.WriteLine("A multiplicação é {0}", resultado);
+        }
+    }
+}
